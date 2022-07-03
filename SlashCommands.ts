e33397fd11f4d80f7da387bc25973cbf2918a5df
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-07-02 23:36:40 -0700

    Update application guild slash commands

--- SlashCommands.ts
+++ SlashCommands.ts
@@ -2,29 +2,31 @@
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9"
 import { token } from "./src/Configs/discord-config.json";
+import GuildConfig from "./src/Configs/guild-config.json";
 
 import { SlashCommandBuilder } from "@discordjs/builders";
-
-const commands = [];
 
 const wordleBotCommands = [
     new SlashCommandBuilder()
         .setName("wstats")
         .setDescription("View your / another person's wordle statistics")
         .addStringOption(user => user
-            .setName("User")
+            .setName("user")
             .setRequired(false)
             .setDescription("@ a user to view their stats")),
 
     new SlashCommandBuilder()
             .setName("wregister")
             .setDescription("Register your discord for Wordle tracking"),
+];
 
-    new SlashCommandBuilder()
-            .setName("wstreak")
-            .setDescription("View your / another person's Wordle streak")
-            .addStringOption(user => user
-                .setName("User")
-                .setRequired(false)
-                .setDescription("@ a user to view their current streak"))
-]
+
+const rest = new REST({ version: "9" }).setToken(token);
+
+
+
+rest.put(Routes.applicationGuildCommands(GuildConfig.client_id, GuildConfig.guild_id), {
+    body: wordleBotCommands
+})
+.then(() => console.log("Successfully registered commands"))
+.catch((err) => console.log(err));
