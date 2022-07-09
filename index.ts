495e12fc7a4aa802563b01adf3a047d7f0b74a85
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-07-09 11:43:43 -0700

    Clean up code
    Move command processing code block for stats in index.ts into a separate function in Stats.ts

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import isWordleMessage from "./src/Utils/WordleMessageRegex";
 
 // Command imports
 import wordleMessage from "./src/Commands/WordleMessage";
-import { allUserStats } from "./src/Commands/Stats";
+import { statsCommand } from "./src/Commands/Stats";
 import { UserStats } from "./src/Types/types";
 
 
@@ -26,58 +26,9 @@ client.on("interactionCreate", async (interaction: Discord.Interaction): Promise
     }
 
     const commandSent = interaction.commandName;
-    const commandArgs = interaction.options;
-
+    
     if (commandSent === "wordle-stats") {
-        let stats: UserStats | undefined;
-
-        if (commandArgs.data.length === 0) {
-            stats = await allUserStats(interaction.user.id);
-        } else {
-            // Do regex matching for ping <@[0-9]+>
-            let optionParam: string | number | boolean | undefined = commandArgs.data[0].value;
-
-            if (optionParam === undefined || typeof optionParam !== "string") {
-                interaction.reply("Invalid user");
-                return;
-            }
-
-            const regex = /^[<][@][!][0-9]+[>]$/;
-
-            if (!regex.test(<string>optionParam)) {
-                interaction.reply("Invalid user");
-                return;
-            }
-
-            let userID: string = optionParam.match(/[0-9]+/)![0]; // There are already numbers in here from regex test above
-
-            // Note: userID is a string of numbers
-            stats = await allUserStats(userID);
-        }
-
-        if (stats === undefined) {
-            interaction.reply({
-                embeds: [new MessageEmbed()
-                    .setColor("DARK_GREY")
-                    .setAuthor({ name: "Wordle Bot" })
-                    .setTitle("No results found")
-                ]
-            });
-            return;
-        }
-
-        const messageEmbed = new MessageEmbed()
-            .setColor("DARK_GREY")
-            .setTitle(`Player Wordle Stats`)
-            .addFields(
-                { name: "Average tries per game", value: `${stats.average_attempts.toFixed(2)}/6`, inline: true },
-                { name: "Success rate", value: `${stats?.success_rate.toFixed(2)}%`, inline: true },
-                { name: '\u200B', value: '\u200B' }, // Line break / empty field for embed
-                { name: "Letter accuracy", value: `${stats?.letter_accuracy.toFixed(2)}%`, inline: true },
-                { name: "Total played", value: `${stats?.total_played}`, inline: true }
-            );
-
-        interaction.reply({ embeds: [messageEmbed] });
+        statsCommand(interaction);
     }
 
 });
--- src/Commands/Stats.ts
+++ src/Commands/Stats.ts
@@ -4,6 +4,61 @@ import { getAvgUserAttempts, getUserTotalBoxCount, getAllAttempts } from "../Dat
 
 // Type imports
 import { BoxCount, DatabaseResponse, UserStats } from "../Types/types"
+import { CommandInteraction, MessageEmbed } from "discord.js";
+
+export async function statsCommand(interaction: CommandInteraction) {
+    let stats: UserStats | undefined;
+
+    const commandArgs = interaction.options;
+
+    if (commandArgs.data.length === 0) {
+        stats = await allUserStats(interaction.user.id);
+    } else {
+        // Do regex matching for ping <@[0-9]+>
+        let optionParam: string | number | boolean | undefined = commandArgs.data[0].value;
+
+        if (optionParam === undefined || typeof optionParam !== "string") {
+            interaction.reply("Invalid user");
+            return;
+        }
+
+        const regex = /^[<][@][!][0-9]+[>]$/;
+
+        if (!regex.test(<string>optionParam)) {
+            interaction.reply("Invalid user");
+            return;
+        }
+
+        let userID: string = optionParam.match(/[0-9]+/)![0]; // There are already numbers in here from regex test above
+
+        // Note: userID is a string of numbers
+        stats = await allUserStats(userID);
+    }
+
+    if (stats === undefined) {
+        interaction.reply({
+            embeds: [new MessageEmbed()
+                .setColor("DARK_GREY")
+                .setAuthor({ name: "Wordle Bot" })
+                .setTitle("No results found")
+            ]
+        });
+        return;
+    }
+
+    const messageEmbed = new MessageEmbed()
+        .setColor("DARK_GREY")
+        .setTitle(`Player Wordle Stats`)
+        .addFields(
+            { name: "Average tries per game", value: `${stats.average_attempts.toFixed(2)}/6`, inline: true },
+            { name: "Success rate", value: `${stats?.success_rate.toFixed(2)}%`, inline: true },
+            { name: '\u200B', value: '\u200B' }, // Line break / empty field for embed
+            { name: "Letter accuracy", value: `${stats?.letter_accuracy.toFixed(2)}%`, inline: true },
+            { name: "Total played", value: `${stats?.total_played}`, inline: true }
+        );
+
+    interaction.reply({ embeds: [messageEmbed] });
+}
 
 export async function allUserStats(userID: string): Promise<UserStats | undefined> {
     let dbRes: DatabaseResponse = await getUserData(userID);
@@ -25,17 +80,6 @@ export async function allUserStats(userID: string): Promise<UserStats | undefine
         all_attempts = [];
     }
 
-    // let statsMessage: Array<string> = [
-    //     `**Stats for:** <@!${userID}>`, 
-    //     `\n**Average Tries:** ${avg_attempts}`,
-    //     // `\n**Letter Accuracy:** ${userBoxCount === null ? 0 : calculateAccuracy(userBoxCount)}`
-    // ];
-
-    // let message: string = "";
-    // for (let i = 0; i < statsMessage.length; i++) {
-    //     message += statsMessage[i];
-    // }
-
     let letter_accuracy: number = (userBoxCount === null) ? 0 : calculateLetterAccuracy(userBoxCount);
     let success_rate: number = (all_attempts === null) ? 0 : calculateSuccessRate(all_attempts);
     return {
@@ -51,17 +95,16 @@ function calculateLetterAccuracy(boxCount: BoxCount): number {
     let accuracy: number = (boxCount.green + boxCount.yellow) / boxCount.total();
     accuracy *= 100;
     return accuracy;
-    // return `${accuracy.toFixed(2)}%`; // Round two two decimals
 }
 
 function calculateSuccessRate(attempts: Array<number>): number {
     if (attempts.length === 0) {
         return 0;
     }
-    
+
     let success: number = 0;
     let fails: number = 0;
-    
+
     attempts.forEach((element: number) => {
         if (element < 7) {
             success++;
